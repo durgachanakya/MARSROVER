faf28f436451a407b3bac1526fd113a6f0a37687
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-08-27 17:51:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
+nasa_image_array = ["nasa1.jpg" , "nasa2.jpg" , "nasa3.jpg" , "nasa4.jpg"];
+
+random_number = Math.floor(Math.random() * 4);
+console.log(random_number);
 rover_width = 100;
 rover_height = 90;
 
-backgrourd_image = "mars.jpg";
+backgrourd_image = nasa_image_array[random_number];
+console.log("background_image = " + backgrourd_image);
 rover_image = "rover.png";
 
 rover_x = 10;
@@ -49,4 +54,44 @@ function my_keydown(e){
         right();
         console.log("right"); 
     }
+}
+
+function up(){
+     if (rover_y >= 0)
+{
+    rover_y = rover_y-10;
+    console.log("when the up arrow is pressed , x = " + rover_x + " - y = " + rover_y);
+    uploadbackground();
+    uploadrover();
+}
+}
+
+function down(){
+    if (rover_y <= 500)
+{
+   rover_y = rover_y + 10;
+   console.log("when the down arrow is pressed , x = " + rover_x + " - y = " + rover_y);
+   uploadbackground();
+   uploadrover();
+}
+}
+
+function right(){
+    if (rover_x <= 700)
+{
+   rover_x = rover_x + 10;
+   console.log("when the right arrow is pressed , x = " + rover_x + " - y = " + rover_y);
+   uploadbackground();
+   uploadrover();
+}
+}
+
+function left(){
+    if (rover_x >= 0)
+{
+   rover_x = rover_x - 10;
+   console.log("when the left arrow is pressed , x = " + rover_x + " - y = " + rover_y);
+   uploadbackground();
+   uploadrover();
+}
 }
